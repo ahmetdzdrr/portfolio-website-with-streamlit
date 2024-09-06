ba4f6f6d4f0763d3c1334b728600d4547726f6e5
Ahmet Dizdar <117534684+ahmetdzdrr@users.noreply.github.com>
2024-09-06 16:56:44 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -63,12 +63,12 @@ with st.sidebar:
 
     st.markdown(
     """
-    <span style="background-color: transparent; text-align: center; margin-top: 20px;">
+    <p style="background-color: transparent; text-align: center; margin-top: 20px; border-radius: 0px !important;">
         <a href="https://www.buymeacoffee.com/ahmetdizdar" target="_blank">
             <img src="https://img.buymeacoffee.com/button-api/?text=Buy%20me%20a%20coffee&emoji=&slug=ahmetdizdar&button_colour=FFDD00&font_colour=000000&font_family=Arial&outline_colour=000000&coffee_colour=ffffff" 
             alt="Buy Me A Coffee" style="box-shadow: none; border: none;">
         </a>
-    </span>
+    </p>
     """, 
     unsafe_allow_html=True)
 
