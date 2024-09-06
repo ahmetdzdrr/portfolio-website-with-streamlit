34db1163db8654a6e4696f2ee4a45cfa8d783beb
Ahmet Dizdar <117534684+ahmetdzdrr@users.noreply.github.com>
2024-09-06 17:02:00 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -59,7 +59,19 @@ with st.sidebar:
     </div>
     """,
     unsafe_allow_html=True
-    )    
+    )
+
+    st.markdown(
+        """
+        <div style="background-color: transparent; text-align: center; margin-top: 20px;">
+            <a href="https://www.buymeacoffee.com/ahmetdizdar" target="_blank">
+                <img src="https://img.buymeacoffee.com/button-api/?text=Buy%20me%20a%20coffee&emoji=&slug=ahmetdizdar&button_colour=FFDD00&font_colour=000000&font_family=Arial&outline_colour=000000&coffee_colour=ffffff" 
+                alt="Buy Me A Coffee" style="box-shadow: none; border: none; width: auto; height: auto;">
+            </a>
+        </div>
+        """, 
+        unsafe_allow_html=True
+    )
 
 if navigation == "Home":
     def typewrite(text:str):
